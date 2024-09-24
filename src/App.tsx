bc9851e3f7ce7bbfbed1bde6a0251b16cb086055
JaeHong <dnflwoghddl@gmail.com>
2024-09-25 00:46:46 +0900

    fix: 코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-const person = {
-  name: 'Gregorio Y. Zara',
-  theme: {
-    backgroundColor: 'black',
-    color: 'pink'
-  }
-};
+import TodoList from 'components/TodoList';
 
-export default function TodoList() {
-  return (
-    <div style={person.theme}>
-      <h1>{person.name}&apos;s Todos</h1>
-      <img
-        className="avatar"
-        src="https://i.imgur.com/7vQD0fPs.jpg"
-        alt="Gregorio Y. Zara"
-      />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-    </div>
-  );
+function App() {
+  return <TodoList />;
 }
+
+export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,25 @@
-const today = new Date();
-
-function formatDate(date: Date) {
-  return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
-}
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
 
 export default function TodoList() {
-  return <h1>To Do List for {formatDate(today)}</h1>;
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}&apos;s TodoList</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
 }
