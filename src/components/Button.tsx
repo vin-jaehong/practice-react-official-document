f13c200990e644c0304626c706972cc868e73ce8
JaeHong <dnflwoghddl@gmail.com>
2024-09-27 01:30:06 +0900

    feat: event handler 바인딩 잘못된 예시

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,10 @@
 export const Button = () => {
   // 이벤트 핸들러 네이밍은 관습적으로 handle + 이벤트 종류 + @
   // camelCase로 작성
-  function handleClick() {
-    alert('You clicked me!');
-  }
+  const handleClick = (text: string) => {
+    alert(text);
+  };
 
-  return <button onClick={handleClick}>Click me</button>;
+  // 아래 이벤트 핸들러 호출부가 랜더링시 바로 실행 됨.
+  return <button onClick={handleClick('test')}>Click me</button>;
 };
