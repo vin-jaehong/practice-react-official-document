34f057d32e1a3b849602faf05ae1f223f73198fe
JaeHong <dnflwoghddl@gmail.com>
2024-09-26 23:25:06 +0900

    test: error case (hook 은 클라이언트 사이드에서만)

--- src/components/TrashCode.tsx
+++ src/components/TrashCode.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
 let guest = 0;
 
 const Cup = () => {
+  const [test, setTest] = useState(0);
+
   // 나쁜 지점: 이미 존재했던 변수를 변경하고 있다!
   guest = guest + 1;
   return <h2>Tea cup for guest #{guest}</h2>;
