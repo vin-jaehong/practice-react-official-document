e0a283aa90f160afa1b7eceb7a64fbc3ccec4379
JaeHong <dnflwoghddl@gmail.com>
2024-10-03 23:51:17 +0900

    feat: forwardRef 를 이용하여 컴포넌트에서 ref 를 전달 받는 방식 예시 + ref 객체에 접근을 제한 하는 기능 예시

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,17 @@
-import { useRef } from 'react';
+import { forwardRef, useRef, useImperativeHandle } from 'react';
+
+const MyInput = forwardRef((props, ref) => {
+  const realInputRef = useRef<HTMLInputElement>(null);
+  useImperativeHandle(ref, () => ({
+    // 오직 focus만 노출
+    focus() {
+      realInputRef.current?.focus();
+    }
+  }));
+  return <input {...props} ref={realInputRef} />;
+});
+
+MyInput.displayName = 'MyInput';
 
 export const Form = () => {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -9,8 +22,10 @@ export const Form = () => {
 
   return (
     <>
-      <input ref={inputRef} />
+      <MyInput ref={inputRef} />
       <button onClick={handleClick}>Focus the input</button>
     </>
   );
 };
+
+Form.displayName = 'Form';
